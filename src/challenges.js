3c841ef830959037434f8585d67ed96a52b3d97b
HailMuffins <rafaelsm123@outlook.com>
2022-06-27 14:50:01 -0300

    Função 5

--- src/challenges.js
+++ src/challenges.js
@@ -26,8 +26,9 @@ function concatName(stringArray) {
 }
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties) {
   // seu código aqui
+  return (wins * 3) + ties;
 }
 
 // Desafio 6
