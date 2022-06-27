59a57b20c99db3b5782db5befafa29aaaa1cc7d0
HailMuffins <rafaelsm123@outlook.com>
2022-06-27 16:36:03 -0300

    Função 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,44 @@
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(numberArray) {
   // seu código aqui
+  if (numberArray.length > 11 || numberArray.length < 11) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  for (let number of numberArray) {
+    if (number < 0 || number > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+
+  for (let i = 0; i < numberArray.length; i++) {
+    let repetitions = 0;
+    for (let j = i; j < numberArray.length; j++) {
+      if (numberArray[i] === numberArray[j]) {
+        repetitions++;
+      }
+    }
+
+    if (repetitions >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  numberArray.splice(0, 0, '(');
+  numberArray.splice(3, 0, ')');
+  numberArray.splice(4, 0, ' ');
+  numberArray.splice(10, 0, '-');
+  return numberArray.join("");
+  //numberArray.splice()
 }
+
+let numbers = [9, 9, 9, 1, 2, 3, 4, 5, 6, 7, 8];
+
+// numbers.splice(0, 0, "(");
+// numbers.splice(3, 0, ")");
+// numbers.splice(4, 0, " ");
+// numbers.splice(10, 0, "-");
+// console.log(numbers.join(""));
+// generatePhoneNumber(numbers);
 
 // Desafio 12
 function triangleCheck() {
